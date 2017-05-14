0f38efc198c192ca7f230c559a50fd46bfe246c2
Admin <admin@Admins-Mac.local>
2017-05-14 21:00:39 +0530

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React        = require('react'),
+    ReactDOM     = require('react-dom');
+// var Route        = require('react-router').Route;
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// Load foundation
+$(document).foundation();
+
+// Load custom css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+      <h1>React Boilerplate3</h1>
+      ,document.getElementById('app')
+    );
