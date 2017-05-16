35cb3d22f7ba84ce0566330c77a499144b13caad
Admin <admin@Admins-Mac.local>
2017-05-16 11:47:51 +0530

    create AddTodo component

---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,36 @@
+var React       = require('react');
+var ReactDOM    = require('react-dom');
+var TestUtils   = require('react-addons-test-utils');
+var expect      = require('expect');
+var $           = require('jQuery');
+
+var AddTodo            = require('AddTodo');
+
+describe('AddTodo', () => {
+  it('should exist', () => {
+    expect(AddTodo).toExist();
+  });
+  
+  it('should call onAddTodo prop with valid data', () => {
+    var todoText = 'check the mail';
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+  
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+    addTodo.refs.todoText.value = todoText;
+    TestUtils.Simulate.submit($el.find('form')[0]);
+    expect(spy).toHaveBeenCalledWith(todoText);
+  });
+
+  it('should not call onAddTodo prop with Invalid input', () => {
+    var todoText = '';
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+  
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todoText.value = todoText;
+    TestUtils.Simulate.submit($el.find('form')[0]);
+    expect(spy).toNotHaveBeenCalled();
+  }); 
+});
